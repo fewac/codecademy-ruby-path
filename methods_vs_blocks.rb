22d9f3571ae638d739d05d2e52f73a03752b4f15
Adriel Marquez <hadriel84@hotmail.com>
2013-10-09 20:52:18 -0500

    Add more examples about making blocks

--- methods_vs_blocks.rb
+++ methods_vs_blocks.rb
@@ -11,3 +11,16 @@ capitalize("mundo")
 ["hola", "mundo"].each do |string|
 	puts "#{string[0].upcase}#{string[1..-1]}" # string[1..-1] => means that ruby prints from second item to the end
 end
+
+#Using code blocks
+
+[1,2,3,4,5].each {|i| puts i}  #first way
+[5,4,3,2,1].each do |i| #second way to do the same thing 
+  puts i
+end
+
+[2,3,4,5,6].each {|playing| puts playing*3} #Doing one thing
+
+[6,5,4,3,2].each do |parametro|
+  puts parametro*3
+end
