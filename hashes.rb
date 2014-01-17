59604f09b5428bc8a2bc6bdd94ae3b7eb20fa6ed
Adriel Marquez <hadriel84@hotmail.com>
2014-01-16 23:18:39 -0600

    adding a NIL example with a little explanation

--- hashes.rb
+++ hashes.rb
@@ -8,7 +8,7 @@ We can create hashes several ways, but two of the most popular are hash literal 
 =end
 
 # Another example could be:
-
+=begin
 data = {"First name" => "Isao", "Last name" => "Nakamura", "Age" => "60"}
 
 # We can also iterate over hashes using the .each, for example (using "data" hash)
@@ -22,4 +22,22 @@ end
 data.each { |key, value|
   puts key, value
 }
+
+# Nil - INTRODUCTION
+# Nil is a ruby's way to say "nothing at all". It's important to realize that "false" and "nil"
+# are not the same thing: "false" means "Not true" while "nil" is nothing at all"".
+# For example: if we have the next hash
+=end
+creatures = {
+  "Weasels" => 0,
+  "platypuses" => 3, 
+  "canaries" => 1,
+  "Heffalumps" => 7,
+  "Tiggers" => 1
+}
+
+# and if we request for a "key" that doesn't exists, we are going to get a "NIL" value:
+
+puts "the value is: #{creatures["leopards"]}"
+
 
